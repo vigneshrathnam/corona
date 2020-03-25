d6d66747dfced8466764b29d9a2d2ae5812f32b2
vignesh015 <vigneshrathnams@gmail.com>
2020-03-26 00:22:00 +0530

    Updates

---
+++ precache-manifest.7d3d58bff95859fa615eb099f0c363b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761e660bbb48b101e0ee546249d0c3a5",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "590f1475ef7e101cfaf1",
+    "url": "/corona/static/css/main.0759064c.chunk.css"
+  },
+  {
+    "revision": "82452208262f96973102",
+    "url": "/corona/static/js/2.4ec574a5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/corona/static/js/2.4ec574a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590f1475ef7e101cfaf1",
+    "url": "/corona/static/js/main.422ceb60.chunk.js"
+  },
+  {
+    "revision": "ea575c59f3d6db2d7662",
+    "url": "/corona/static/js/runtime-main.2116c812.js"
+  }
+]);
